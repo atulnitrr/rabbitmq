594e6365096cdc92f8f5f66d13faecf251c6d0c7
atulnitrr <atulkumar458@gmail.com>
2019-07-27 01:05:31 +0530

    Routing

--- src/main/java/com/rabbitmq/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/rabbitmq/rabbitmq/RabbitmqApplication.java
@@ -1,10 +1,13 @@
 package com.rabbitmq.rabbitmq;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqApplication {
+public class RabbitmqApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqApplication.class, args);
@@ -12,4 +15,11 @@ public class RabbitmqApplication {
 	}
 
 
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Override
+	public void run(final String... args) throws Exception {
+		rabbitTemplate.convertAndSend("TestExchange", "testRouting", "Testing exchnage ");
+	}
 }
